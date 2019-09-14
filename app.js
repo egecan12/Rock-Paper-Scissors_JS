e95e74a7ad636276b9bbdd29b45f9aa4354e2082
Egecan Kahyaoglu <45043515+egecan12@users.noreply.github.com>
2019-09-14 19:26:33 -0400

    up

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 function game() {
     let playerScore = 0;
     let computerScore = 0;
-
+    let playerChoice;
     //start the game
     function startGame() {
         const playButton = document.querySelector("#playButton");
         const introScene = document.querySelector(".intro");
         const matchUp = document.querySelector(".matchUp");
+
+        //3 buttons
+        const rockBtn = document.querySelector(".rock");
+        const paperBtn = document.querySelector(".paper");
+        const scissorsBtn = document.querySelector(".scissors");
+
+        rockBtn.addEventListener("click", function rockPick() {
+            playerChoice = "Rock";
+            console.log(playerChoice);
+
+
+        })
+
+        paperBtn.addEventListener("click", function paperPick() {
+            playerChoice = "Paper";
+            console.log(playerChoice);
+
+
+        })
+        scissorsBtn.addEventListener("click", function scisPick() {
+            playerChoice = "Scissors";
+            console.log(playerChoice);
+
+
+        })
+
+
 
 
         function ignore() {
@@ -19,7 +46,131 @@ function game() {
     //Play Match 
     function playMatch() {
 
-        const preferences = document.querySelectorAll('.preferences button');
+
+        const preferences = document.querySelectorAll(".preferences button");
+        const player_hand = document.querySelector(".player_hand");
+        const computer_hand = document.querySelector(".pc_hand");
+
+        //Computer options 
+
+        const computerOptions = ["Rock", "Paper", "Scissors"];
+
+        let randomNumber = Math.floor(Math.random() * 3);
+
+        let pcChoice = computerOptions[randomNumber];
+
+
+
+        function getTheImage() {
+
+            switch (pcChoice) {
+                case "Paper":
+                    computer_hand.src = "paper.png";
+                    break;
+                case "Rock":
+                    computer_hand.src = "rock.png";
+                    break;
+                case "Scissors":
+                    computer_hand.src = "scissors.png";
+                default:
+                    break;
+            }
+
+
+
+        }
+
+
+
+
+        function getPlayerImage() {
+
+
+            switch (playerChoice) {
+                case "Paper":
+                    player_hand.src = "paper.png";
+                    break;
+                case "Rock":
+                    player_hand.src = "rock.png";
+                    break;
+                case "Scissors":
+                    player_hand.src = "scissors.png";
+                default:
+                    break;
+            }
+
+
+        }
+
+
+
+
+
+        //buttons
+        preferences.forEach(preference => {
+            preference.addEventListener('click', getTheImage);
+        });
+
+
+        // Comparing hands
+        function compareHands(pcChoice, playerChoice) {
+
+            let winner = document.querySelector(".winner");
+            //check for tie
+            if (playerChoice === pcChoice) {
+
+                winner.textContent = " Ohh tie !"
+                return;
+            }
+            //  check for rock 
+            if (playerChoice === "Rock") {
+
+                if (pcChoice === "Scissors") {
+
+                    winner.textContent = " Good job Lets do it again !"
+                    return;
+                } else {
+
+                    winner.textContent = " Hahahahahahah Loser !"
+                    return;
+                }
+
+            }
+
+
+            //chek for paper           
+            if (playerChoice === "Paper") {
+
+                if (pcChoice === "Rock") {
+
+                    winner.textContent = " Good job Lets do it again !"
+                    return;
+                } else {
+
+                    winner.textContent = " Hahahahahahah Loser !"
+                    return;
+
+                }
+
+            }
+
+
+            //check for scissors
+            if (playerChoice === "Scissors") {
+
+                if (pcChoice === "Paper") {
+
+                    winner.textContent = " Good job Lets do it again !"
+                    return;
+                } else {
+
+                    winner.textContent = " Hahahahahahah Loser !"
+                    return;
+                }
+
+            }
+
+        }
     }
 
 
@@ -30,6 +181,7 @@ function game() {
     // I gotta call all inner functions over here to make them work
     startGame();
     playMatch();
+    compareHands();
 
 
 }
